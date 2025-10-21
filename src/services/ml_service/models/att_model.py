b15b1dcab52c7a4b6c2c18736238cd1f7f917ffc
Danil Moiseenk <danil.moiseenko@icloud.com>
2025-10-21 14:59:29 +0500

    add models for ml-service

---
+++ src/services/ml_service/models/att_model.py
@@ -0,0 +1,30 @@
+import logging
+import whisper
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class BaseSpechRecognitionModel:
+    def __init__(self):
+        pass
+    
+    def __call__(self, path: str):
+        return {'status': True, 'error': 'Error description', 'text': 'some text'}
+    
+
+class WhisperSpeechRecognitionModel(BaseSpechRecognitionModel):
+    def __init__(self, model_name="tiny"):
+        super().__init__()
+        self.model = whisper.load_model(model_name)
+        
+        self.license = 'MIT'
+    
+    def __call__(self, path: str):
+        try:
+            result = self.model.transcribe(path)
+            return {'status': True, 'text': result['text']}
+        except Exception as e:
+            return {'status': False, 'error': str(e)}
+
