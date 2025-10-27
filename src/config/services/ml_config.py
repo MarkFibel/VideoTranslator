7c73d293679ed5318ab955e1bf0631f871cf9ae8
Andrey <andrey19080308-12@mail.ru>
2025-10-27 20:56:26 +0500

    upd ml_service config

--- src/config/services/ml_config.py
+++ src/config/services/ml_config.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from pydantic import Field
 from pydantic_settings import BaseSettings
 
 load_dotenv()
@@ -8,7 +9,7 @@ class Settings(BaseSettings):
         env_file = ".env"
         case_sensitive = True
     
-    RPC_ENABLED: bool = True
+    RPC_ENABLED: bool = Field(default=True, exclude=True)
     
     TEMP_DIR: str = 'var/temp'
     MODEL_CACHE_DIR: str = 'var/model_cache'
